f39f4cfbeb39b1cca33a9df10670e15b879aceb1
unknown <rizal.h33@gmail.com>
2026-04-23 20:28:33 +0700

    fix logo

--- components/guru/SidebarClient.tsx
+++ components/guru/SidebarClient.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { Profile } from "@/types";
 import clsx from "clsx";
-
+import Image from 'next/image';
 
 const NAV = [
   { href: "/guru/dashboard",    icon: "📊", label: "Dashboard" },
